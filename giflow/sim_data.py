e4f1c158883f4ed1274ac2c10610e2068cb02107
Henrietta Rakoczi <105510764+rakoczi-h@users.noreply.github.com>
2023-11-10 17:06:02 +0000

    Update sim_data.py

--- giflow/sim_data.py
+++ giflow/sim_data.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import json
 import numpy as np
+import os
+import sys
 
 from lib.make_data import make_2D_grid, make_3D_grid_limits, make_parameterised_box_dataset, make_voxelised_box_dataset, make_3D_grid, make_box_params
 start_time  = datetime.now()
