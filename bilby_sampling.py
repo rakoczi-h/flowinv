3b5b327c1f648a5e365b1a777676ce94c7b20c97
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2024-06-05 13:42:09 +0100

    changed some things to make it applicable to the local file infrastructure

--- bilby_sampling.py
+++ bilby_sampling.py
@@ -1,3 +1,4 @@
+#!/scratch/wiay/2263373r/masters/conda_envs/flowenv/bin/python
 import bilby
 import h5py
 import json
@@ -7,16 +8,21 @@ import os
 import shutil
 import sys
 import pickle as pkl
+import pandas as pd
 
 from giflow.box import Box
+from giflow.survey import GravitySurvey
 
-n = int(sys.argv[1]) # Give the index of the testcase as an input to the script
+n = int(sys.argv[1])
 
 label = "inversion"
-bilby_outdir = "/bilby_results/"
+bilby_outdir = "/data/www.astro/2263373r/giflow/bilby/box/normalised/"
+bilby.utils.check_directory_exists_and_if_not_mkdir(bilby_outdir)
 
-data_loc = '/data/'
-data_name= 'testset.pkl'
+outdir = os.path.join(bilby_outdir, f"testcase_{n}/")
+bilby.utils.check_directory_exists_and_if_not_mkdir(outdir)
+
+data_loc = '/data/wiay/2263373r/giflow/box/parameterised/normalised/'
 
 # ----------------------- Functions -------------------------------
 def model(survey_coordinates, px, py, pz, lx, ly, lz, alpha):
@@ -45,22 +51,27 @@ def prior(keys, distributions):
     return priors
 
 # --------------------- Reading data -----------------------------
-with open(os.path.join(data_loc, data_name), 'rb') as file:
-    dt = pkl.load(file)
-box = dt.boxes[n]
-survey = dt.surveys[n]
+with open(os.path.join(data_loc, "testset.pkl"), 'rb') as file:
+    dt_test = pkl.load(file)
+box = dt_test.boxes[n]
+survey = dt_test.surveys[n]
+keys = dt_test.parameter_labels
+
+survey.plot_pixels(filename=os.path.join(outdir, "survey.png"), include_noise=True)
+
+data = survey.gravity+survey.noise
+sigma = survey.noise_scale
 survey_coordinates = survey.survey_coordinates
-data = survey.gravity + survey.noise
-sigma = dt.survey_framework['noise_scale']
-keys = box.parameter_labels
 
-outdir = bilby_outdir + f"testcase_{n}/"
-bilby.utils.check_directory_exists_and_if_not_mkdir(outdir)
+modelled_gravity = model(survey_coordinates, px=box.px, py=box.py, pz=box.pz, lx=box.lx, ly=box.ly, lz=box.lz, alpha=box.alpha)
 
+survey.gravity = modelled_gravity
+
+survey.plot_pixels(filename=os.path.join(outdir, "survey_modelled.png"), include_noise=True)
 # --------------------- Defining sampler inputs ------------------
 
 # PRIOR
-priors = prior(dt.priors.keys, dt.priors.distributions)
+priors = prior(dt_test.priors.keys, dt_test.priors.distributions)
 # TRUTH
 injection_parameters = dict.fromkeys(keys)
 for idx, k in enumerate(keys):
