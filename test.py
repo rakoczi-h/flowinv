90cd3eb176e6688a3045663bbad1f84f8bd53a68
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2025-02-25 11:32:07 +0000

    bla

--- test.py
+++ test.py
@@ -14,13 +14,13 @@ from giflow.read_files import read_files
 from giflow.latent import FlowLatent
 
 #n = int(sys.argv[1])
-#survey_coordinates_to_include = []
-survey_coordinates_to_include = ['x', 'y', 'noise_scale']
+survey_coordinates_to_include = []
+#survey_coordinates_to_include = ['x', 'y', 'noise_scale']
 model_info_to_include= []
 mix_survey_order = False
-#bilby_location = '/data/www.astro/2263373r/giflow/4_paper/bilby/'
-bilby_location = None
-flow_location = f"/data/www.astro/2263373r/giflow/4_paper/narrow_volume/combined/run_2025-01-23 12:10:10.004675/"
+bilby_location = '/data/www.astro/2263373r/giflow/4_paper/bilby/'
+#bilby_location = None
+flow_location = f"/data/www.astro/2263373r/giflow/4_paper/parameterised/run_2024-07-26 13:50:53.730077/"
 
 #directories = []
 #for roots, dirs, files in os.walk(flow_location):
@@ -37,7 +37,7 @@ flow.load(flow_location)
 flow.flowmodel.to(device)
 flow.save_location = flow_location
 
-#flow.data_location = '/scratch/balta1/2263373r/4_paper/combined/'
+flow.data_location = '/scratch/balta1/2263373r/4_paper/parameterised/'
 data_location = flow.data_location
 
 # -------------------- Validation data --------------
@@ -45,8 +45,10 @@ with open(os.path.join(flow.data_location, "validationset_0.pkl"), 'rb') as file
 #with open("/data/wiay/2263373r/giflow/box/qinetiq_dummy_set.pkl", 'rb') as file:
     dt_val = pkl.load(file)
 keys = dt_val.parameter_labels
-#labels = [r'$c_x$', r'$c_y$', r'$c_z$', r'$l_x$', r'$l_y$', r'$l_z$', r'$\alpha$']
-labels= keys
+
+labels = [r'$c_x$', r'$c_y$', r'$c_z$', r'$l_x$', r'$l_y$', r'$l_z$', r'$\alpha$']
+#labels = [r'$l_x$', r'$l_y$']
+#labels= keys
 priors = dt_val.priors
 
 prior_bounds = []
@@ -89,40 +91,40 @@ results = []
 for i in range(testsize):
     samples, log_probabilities = flow.sample_and_logprob(test_dataset.tensors[1][i], num=2000)
     result = BoxFlowResults(samples=samples, conditional=[test_conditional[j][i] for j in range(len(test_conditional))], log_probabilities=log_probabilities, true_parameters=np.array([test_data[0][i]]), parameter_labels=labels, survey_coordinates=dt_test.surveys[0].survey_coordinates)
-    result.directory = os.path.join(flow_location, f"testcase_{i}/")
+    result.directory = os.path.join(flow_location, f"testcase_to_present_{i}/")
     result.samples_to_csv()
     dt_test.surveys[i].plot_contours(filename=os.path.join(result.directory, "survey.png"), include_noise=True, axis_limits=[-0.4375, 0.4375, -0.4375, 0.4375])
     results.append(result)
 
 # ----------------- Consistency tests --------------------------
-# P-P TEST
-flow.pp_test(validation_dataset=pp_dataset,
-    parameter_labels = labels,
-    #parameter_labels=[r'$q_1$', r'$q_2$', r'$q_3$', r'$q_4$', r'$q_5$', r'$q_6$', r'$q_7$', r'$q_8$', r'$q_9$', r'$q_10$']
-)
+## P-P TEST
+#flow.pp_test(validation_dataset=pp_dataset,
+#    parameter_labels = labels,
+#    #parameter_labels=[r'$q_1$', r'$q_2$', r'$q_3$', r'$q_4$', r'$q_5$', r'$q_6$', r'$q_7$', r'$q_8$', r'$q_9$', r'$q_10$']
+#)
 
-# CORNER PLOTS                              
-for i, result in enumerate(results):
-    result.corner_plot(filename="corner_plot.png")
-    result.corner_plot(filename="corner_plot_with_prior_bounds.png", prior_bounds=prior_bounds)
-    print(f"Made {i+1}/{testsize} corner plots.")
+## CORNER PLOTS                              
+#for i, result in enumerate(results):
+#    result.corner_plot(filename="corner_plot_v2.png")
+#    result.corner_plot(filename="corner_plot_with_prior_bounds_v2.png", prior_bounds=prior_bounds)
+#    print(f"Made {i+1}/{testsize} corner plots.")
 
-# SURVEY CONSISTENCY
-for i, result in enumerate(results):
-    result.plot_compare_surveys(model_framework=dt_test.model_framework, filename="compare_survey.png", include_examples=True)
-    print(f"Made {i+1}/{testsize} survey comparison plots.")
-
-# VOXELISED MODEL COMPARISON
-for i, result in enumerate(results):
-    if not i == 2:
-        continue
-    #result.true_parameters = voxelised_model[np.newaxis,...]
-    result.plot_compare_voxel_slices(filename=f"compare_voxel_slices.png",
-        plot_truth=True,
-        normalisation=[-2500.0, 500.0], 
-        model_framework=dt_test.model_framework,
-        slice_coords=[1, 4, 8])
-    print(f"Made {i+1}/{testsize} voxel slice comparison plots.")
+## SURVEY CONSISTENCY
+#for i, result in enumerate(results):
+#    result.plot_compare_surveys(model_framework=dt_test.model_framework, filename="compare_survey.png", include_examples=True)
+#    print(f"Made {i+1}/{testsize} survey comparison plots.")
+#
+## VOXELISED MODEL COMPARISON
+#for i, result in enumerate(results):
+#    if not i == 2:
+#        continue
+#    #result.true_parameters = voxelised_model[np.newaxis,...]
+#    result.plot_compare_voxel_slices(filename=f"compare_voxel_slices.png",
+#        plot_truth=True,
+#        normalisation=[-2500.0, 500.0], 
+#        model_framework=dt_test.model_framework,
+#        slice_coords=[1, 4, 8])
+#    print(f"Made {i+1}/{testsize} voxel slice comparison plots.")
 #
 ## 3D PLOT COMPARISON PLOT
 #for i, result in enumerate(results):
@@ -178,8 +180,9 @@ if bilby_location is not None:
             for key in keys:
                 bilby_samples.append(bilby_posterior_dict[key])
             bilby_samples = np.array(bilby_samples).T
-        result.overlaid_corner(bilby_samples, ['NS', 'NF'], filename="overlaid_corner_plot.png")
+        result.overlaid_corner(bilby_samples, ['NS', 'NF'], filename="overlaid_corner_plot_full.png")
+        result.overlaid_corner(bilby_samples, ['NS', 'NF'], filename="overlaid_corner_plot_prior_bounds_full.png", prior_bounds=prior_bounds)
         js = result.get_js_divergence(bilby_samples)
-        df = pd.DataFrame(data={'js': js})
-        df.to_csv(os.path.join(result.directory, 'js.csv'))
+        #df = pd.DataFrame(data={'js': js})
+        #df.to_csv(os.path.join(result.directory, 'js.csv'))
         print(js)
