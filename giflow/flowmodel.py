Fixed data scaling output so that it is consistent with require data shapes

giflow/flowmodel.py
@@ -398,8 +398,7 @@ class FlowModel():
             end_sample = datetime.now()
         print(f"{num} samples drawn. Time taken: \t {end_sample-start_sample}")
         s = s.cpu().numpy()
-
-        s = self.scalers['data'].inv_scale_data(s)[0]
+        s = self.scalers['data'].inv_scale_data(s)
         l = l.cpu().numpy()
         return s, l
 
@@ -430,12 +429,13 @@ class FlowModel():
                 The combined p values.
         """
         truths = validation_dataset.tensors[0][:int(num_cases)].cpu().numpy()
-        truths = self.scalers['data'].inv_scale_data(truths)[0]
-        if np.shape(truths)[1] > num_params:
+        truths = self.scalers['data'].inv_scale_data(truths)
+        if len(truths) > num_params:
             indices = np.random.randint(np.shape(truths)[1], size=num_params)
         else:
-            num_params = np.shape(truths)[1]
+            num_params = len(truths)
             indices = np.arange(0, num_params)
+        print(indices)
         if parameter_labels == None:
             parameter_labels = [f"q{x}" for x in range(num_params)] # number of parameters to get the posterior for (will be 512)
         posteriors = []
@@ -446,8 +446,8 @@ class FlowModel():
                 injection = dict()
                 x, _ = self.sample_and_logprob(conditional=validation_dataset.tensors[1][cnt], num=num_samples)
                 for i, key in enumerate(parameter_labels):
-                    posterior[key] = x[:,indices[i]]
-                    injection[key] = truths[cnt,indices[i]]
+                    posterior[key] = x[i][:,0]
+                    injection[key] = truths[indices[i]][cnt,0]
                 posterior = pd.DataFrame(posterior)
                 posteriors.append(posterior)
                 injections.append(injection)