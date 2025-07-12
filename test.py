26452bbdac1e7af090dcf52b6ec1c5438df28d2b
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2025-07-12 09:05:43 +0100

    New test case

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import os
 import pickle as pkl
 import pandas as pd
 
-from giflow.results import BoxFlowResults
+from giflow.results import FlowResults
 from giflow.flowmodel import FlowModel
+from giflow.datareader import DataReader
 
-flow_location = 'trained_flow'
+flow_location = '/data/www.astro/2263373r/fault_python_version/5_parameter_test/run_2025-07-07 08:08:14.088743/'
 
 #Reading the flow
 device = torch.device('cuda')
@@ -17,64 +18,65 @@ flow.flowmodel.to(device)
 flow.save_location = flow_location
 data = flow.data_location
 
-# Validation data
-valsize = 500 
-with open(os.path.join(data, 'valset.pkl'), 'rb') as file:
-    dt_val = pkl.load(file)
-    validation_data, validation_conditional = dt_val.make_data_for_network(
-        survey_coordinates_to_include = ['noise_scale'],
-        model_info_to_include = [],
-        add_noise = True,
-        mix_survey_order = False,
-    )
-validation_dataset = flow.make_tensor_dataset(validation_data, validation_conditional, device=device, scale=True)
+# ------------------- DATA -------------------------
+model_info_to_include = ['cx', 'cy', 'l', 'alpha', 'cz']
+labels = [r'$c_x$', r'$c_y$', r'$l$', r'$\alpha$', r'$c_z$']
+survey_info_to_include = []
+# Reading in files
+ppsize = 100
+dr_pp = DataReader(filenames="ppset_1.pkl", data_location=data, model_info_to_include=model_info_to_include, survey_info_to_include=survey_info_to_include, datasize=ppsize)
+pp_data, pp_conditional = dr_pp.read_files()
 
-# Test data
-testsize = 2
-with open(os.path.join(data, 'testset.pkl'), 'rb') as file:
-    dt_test = pkl.load(file)
-    test_data, test_conditional = dt_test.make_data_for_network(
-        survey_coordinates_to_include = ['noise_scale'],
-        model_info_to_include = [],
-        add_noise = True,
-        mix_survey_order = False,
-    )
+pp_dataset = flow.make_tensor_dataset(pp_data, pp_conditional, device=device, scale=True)
+
+testsize = 1
+dr_test = DataReader(filenames="testset_1.pkl", data_location=data, model_info_to_include=model_info_to_include, survey_info_to_include=survey_info_to_include, datasize=testsize)
+test_data, test_conditional = dr_test.read_files()
+
 test_dataset = flow.make_tensor_dataset(test_data, test_conditional, device=device, scale=True)
+
+with open(os.path.join(data, 'trainset_1.pkl'), 'rb') as file:
+    dt_test = pkl.load(file)
+print(dt_test.sourcemodels[0].parameters)
 
 # --------------- TESTING -------------------
 # P-P plot
-flow.pp_test(validation_dataset=validation_dataset,
-             parameter_labels=dt_val.parameter_labels)
+flow.pp_test(validation_dataset=pp_dataset,
+             parameter_labels=labels)
+
 
 # Generating results for some test data
 results = []
 for i in range(testsize):
     samples, log_probabilities = flow.sample_and_logprob(test_dataset.tensors[1][i], # the conditional
                                                          num=2000) # number of samples we want to draw
-    result = BoxFlowResults(samples=samples,
+    samples = np.array([s[:,0] for s in samples]).T
+    print(np.shape(samples))
+    result = FlowResults(samples=samples,
                             conditional=[test_conditional[j][i] for j in range(len(test_conditional))],
                             log_probabilities=log_probabilities,
-                            true_parameters=np.array([test_data[0][i]]),
-                            parameter_labels=dt_val.parameter_labels,
+                            true_parameters=np.array([td[i,0] for td in test_data]).T,
+                            parameter_labels=labels,
                             survey_coordinates=dt_test.surveys[0].survey_coordinates
                            )
     results.append(result)
 
-    result.directory = os.path.join(flow_location, f"testcase_{i}/")
+    result.directory = os.path.join(flow_location, f"testcase_bilby_{i}/")
     # plotting the surveys we are inverting
     dt_test.surveys[i].plot_contours(filename=os.path.join(result.directory, "survey.png"), include_noise=True)
+    dt_test.surveys[i].plot_pixels(filename=os.path.join(result.directory, "survey.png"), include_noise=True)
 
 # CORNER PLOTS                              
 for i, result in enumerate(results):
     result.corner_plot(filename="corner_plot.png")
 
 # VOXELISED MODEL COMPARISON
-dt_test.model_framework['ranges'] = [[-0.75, 0.75], [-0.75, 0.75], [-1.5, 0.0]]
-dt_test.model_framework['grid_shape'] = [10, 10, 10]
-
-for i, result in enumerate(results):
-    result.plot_compare_voxel_slices(filename=f"compare_voxel_slices.png",
-                                     plot_truth=True,
-                                     normalisation=[-2500.0, 500.0],
-                                     model_framework=dt_test.model_framework,
-                                     slice_coords=[1, 4, 8])
+#dt_test.model_framework['ranges'] = [[-0.75, 0.75], [-0.75, 0.75], [-1.5, 0.0]]
+#dt_test.model_framework['grid_shape'] = [10, 10, 10]
+#
+#for i, result in enumerate(results):
+#    result.plot_compare_voxel_slices(filename=f"compare_voxel_slices.png",
+#                                     plot_truth=True,
+#                                     normalisation=[-2500.0, 500.0],
+#                                     model_framework=dt_test.model_framework,
+#                                     slice_coords=[1, 4, 8])
--- train.py
+++ train.py
@@ -2,60 +2,57 @@ import os
 from sklearn.preprocessing import MinMaxScaler
 import torch
 import pickle as pkl
+import numpy as np
+from datetime import datetime
 
 from giflow.box import BoxDataset
 from giflow.scaler import Scaler
 from giflow.flowmodel import FlowModel, save_flow
+from giflow.datareader import DataReader
 
 # Defining directories
-data = 'data' # where our training and validation that are located
-save = 'trained_flow' # where we want to save our outputs
+data = '/scratch/balta0/2263373r/fault_python/' # where our training and validation that are located
+save = '/data/www.astro/2263373r/fault_python_version/5_parameter_test/' # where we want to save our outputs
+save = os.path.join(save, f"run_{datetime.now()}/")
 
 if not os.path.exists(save):
     os.mkdir(save)
 
 # ------------------- DATA -------------------------
+model_info_to_include = ['cx', 'cy', 'l', 'alpha', 'cz']
+survey_info_to_include = []
 # Reading in files
-trainsize = 5000
-with open(os.path.join(data, 'trainset.pkl'), 'rb') as file:
-    dt = pkl.load(file)
-    train_data, train_conditional = dt.make_data_for_network(
-        survey_coordinates_to_include = ['noise_scale'],
-        model_info_to_include = [],
-        add_noise = True, # This refers to survey noise.
-        mix_survey_order = False
-    )
-
-# Need to do this with the validation data too
-valsize = 500
-with open(os.path.join(data, 'valset.pkl'), 'rb') as file:
-    dt = pkl.load(file)
-    validation_data, validation_conditional = dt.make_data_for_network(
-        survey_coordinates_to_include = ['noise_scale'],
-        model_info_to_include = [],
-        add_noise = True,
-        mix_survey_order = False,
-    )
+trainsize = 500000
+dr_train = DataReader(filenames=[f"trainset_{n}.pkl" for n in range(1,6)], data_location=data, model_info_to_include=model_info_to_include, survey_info_to_include=survey_info_to_include, datasize=trainsize)
+train_data, train_conditional = dr_train.read_files()
 
 # Scaling the data
-sc_data = Scaler(scalers = [MinMaxScaler()]) # Need to define the scaler for each element in the train_data list.
+sc_data = Scaler(scalers = [MinMaxScaler(), MinMaxScaler(), MinMaxScaler(), MinMaxScaler(), MinMaxScaler()]) # Need to define the scaler for each element in the train_data list.
 sc_data.scale_data(train_data, fit = True) # Fit the scaler and store in the class
 
-sc_conditional = Scaler(scalers = [MinMaxScaler(), MinMaxScaler()])
+sc_conditional = Scaler(scalers = [MinMaxScaler()])
 sc_conditional.scale_data(train_conditional, fit = True)
 
 scalers = {'conditional': sc_conditional, 'data': sc_data}
 
+trainsize = 1500000
+dr_train = DataReader(filenames=[f"trainset_{n}.pkl" for n in range(1,16)], data_location=data, model_info_to_include=model_info_to_include, survey_info_to_include=survey_info_to_include, datasize=trainsize)
+train_data, train_conditional = dr_train.read_files()
+
+valsize = 150000
+dr_train = DataReader(filenames=[f"validationset_{n}.pkl" for n in range(1,16)], data_location=data, model_info_to_include=model_info_to_include, survey_info_to_include=survey_info_to_include, datasize=valsize)
+validation_data, validation_conditional = dr_train.read_files()
+
 # ------------------ FLOW --------------------------
 # Defining the flow parameters
 hyperparameters = {
-        'n_inputs': 7, # the total number of parameters in the source model, including any additional information we chose to include
-        'n_conditional_inputs': 65, # the total number of values in the conditional
-        'n_transforms': 12,
+        'n_inputs': 5, # the total number of parameters in the source model, including any additional information we chose to include
+        'n_conditional_inputs': 2500, # the total number of values in the conditional
+        'n_transforms': 6,
         'n_blocks_per_transform': 2,
-        'n_neurons': 64,
+        'n_neurons': 16,
         # The parameters below define some settings for the training
-        'batch_size': 5000,
+        'batch_size': 1000,
         'batch_norm': True,
         'lr': 0.001,
         'epochs': 3000,
@@ -78,6 +75,8 @@ optimiser = torch.optim.Adam(
     flow.flowmodel.parameters(),
     lr = flow.hyperparameters['lr']
 )
+
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimiser, factor=0.1, patience=100, threshold=1e-6, cooldown=10)
 
 # Specifying the GPU
 device = torch.device('cuda')
@@ -103,7 +102,7 @@ flow.train(
     optimiser = optimiser,
     validation_dataset = validation_dataset,
     train_dataset = train_dataset,
-    scheduler = None,
+    scheduler = scheduler,
     device = device
 )
 
