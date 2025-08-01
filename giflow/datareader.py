174b8c8ce4444736ddc0dddd3d29830c33df48a8
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2025-08-01 10:33:07 +0200

    Added documentation

--- giflow/datareader.py
+++ giflow/datareader.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 
 from .prior import Prior
-from .dataset import FaultDataset
+from .dataset import Dataset
 
 class DataReader():
     """
@@ -50,6 +50,21 @@ class DataReader():
         super().__setattr__(name, value)
 
     def split_filenames(self, chunk_size=None, randomise=False):
+        """
+        Function to slip the list of filenames into chunks. Useful when want to read data in batches, not all at once.
+        Parameters:
+        -----------
+            chunk_size : int
+                The size of each section of filenames.
+                If not given, then read from the class. 
+                If none, then the whole list of filenames is returned as one list. (Default: None)
+            randomise: bool
+                If true, the list of filenames is shuffled
+        Output:
+        -------
+            filenames: list
+                List of lists, with each sublist of length chunk_size. 
+        """
         if chunk_size is not None:
             self.chunk_size = chunk_size
         if self.chunk_size is None:
@@ -86,6 +101,10 @@ class DataReader():
                 If True, then the read data is reused with different noise realisations.
             noise_augment_factor: int
                 If noise_augment is True, this is the number of times the same data is reused with different noise realisations.
+            noise_seed: int
+                If given, then the noise for each gravity survey within the dataset will be generated with that seed. (Default: None)
+            noise_distribution: Prior
+                If it is given, the noise scale for each gravity survey will be drawn from this distribution. Has to be a Prior object. (Default: None)
         Output
         ------
             train_data: list
@@ -99,8 +118,13 @@ class DataReader():
             with open(os.path.join(self.data_location, f), 'rb') as file:
                 dt = pkl.load(file)
                 # Reading files containing FaultDataset objects
-                if isinstance(dt, FaultDataset):
-                    td, tc = dt.make_data_for_network(survey_info_to_include=self.survey_info_to_include, model_info_to_include=self.model_info_to_include, add_noise=True, noise_seed=noise_seed, noise_distribution=noise_distribution)   # these objects have corresponding method to format the data that is compatible with training
+                if isinstance(dt, Dataset):
+                    # these objects have corresponding method to format the data that is compatible with training
+                    td, tc = dt.make_data_for_network(survey_info_to_include=self.survey_info_to_include, 
+                                                      model_info_to_include=self.model_info_to_include, 
+                                                      add_noise=True, 
+                                                      noise_seed=noise_seed, 
+                                                      noise_distribution=noise_distribution)
                     train_data.append(td)
                     train_conditional.append(tc)
                     # Noise augmentation
@@ -109,17 +133,11 @@ class DataReader():
                             raise ValueError('Need to provide the noise scale to the class')
                         for i in range(noise_augment_factor-1):
                             self.regenerate_noise(dt)
-                            td, tc = dt.make_data_for_network(survey_info_to_include=self.survey_info_to_include, model_info_to_include=self.model_info_to_include, add_noise=True, noise_seed=noise_seed, noise_distribution=noise_distribution)
-                            train_data.append(td)
-                            train_conditional.append(tc)
-                # Reading files containing dictionaries
-                elif isinstance(dt, dict):
-                    td, tc = self.read_dictionary(dt)
-                    train_data.append(td)
-                    train_conditional.append(tc)
-                    if noise_augment:
-                        for i in range(noise_augment_factor-1):
-                            td, tc = self.read_dictionary(dt, make_noise=True)
+                            td, tc = dt.make_data_for_network(survey_info_to_include=self.survey_info_to_include,
+                                                            model_info_to_include=self.model_info_to_include,
+                                                            add_noise=True, 
+                                                            noise_seed=noise_seed, 
+                                                            noise_distribution=noise_distribution)
                             train_data.append(td)
                             train_conditional.append(tc)
                 else:
@@ -157,69 +175,4 @@ class DataReader():
 
         print(f"Dataset read. Data size = {self.datasize}")
         return train_data, train_conditional
-
-#    def read_dictionary(self, dt, make_noise=True):
-#        """
-#        Method to extract relevant information from a dictionary.
-#        Parameters
-#        ----------
-#            dt: dict
-#                The dictionary to read.
-#            make_noise: bool
-#                If True, then noise is made from the given noise_scale.
-#        """
-#
-#        if not isinstance(dt, dict):
-#            raise ValueError('Input has to be a dictionary.')
-#        if not make_noise:
-#            if dt['noise'] is None:
-#                print('No noise in dictionary, making noise.')
-#                make_noise = True
-#
-#        # Making noise
-#        if make_noise:
-#            if self.noise_scale is None:
-#                raise ValueError('Either provide the noise_scale to the class, or set make_noise to False.')
-#            noise_prior = Prior(distributions={"noise_scale" : self.noise_scale})
-#            noise_scale_sampled = noise_prior.sample(size=(np.shape(dt['gravity'])[0],1), returntype='dict')['noise_scale']
-#            gravity = []
-#            for i, n in enumerate(noise_scale_sampled):
-#                gravity.append(dt['gravity'][i]+np.random.normal(0.0, n, size=np.shape(dt['gravity'])[1]))
-#            gravity = np.vstack(gravity)
-#        # Using noise from within the dictionary
-#        else:
-#            gravity = []
-#            for g in dt['gravity']:
-#                gravity.append(g+dt['noise'][i])
-#            gravity = np.vstack(gravity)
-#
-#        tc = [gravity]
-#
-#        # Making the conditional
-#        if self.survey_coordinates_to_include is not None:
-#            labels = ['x', 'y', 'z']
-#            for idx, label in enumerate(labels):
-#                if any([l==label for l in self.survey_info_to_include]):
-#                    if not label in dt:
-#                        raise ValueError(f"{label} can't be found in dictionary.")
-#                    tc.append(dt[label])
-#            labels = ['x_ranges', 'y_ranges', 'z_ranges']
-#            for idx, label in enumerate(labels):
-#                if any([l==label for l in self.survey_info_to_include]):
-#                    if not 'survey_ranges' in dt:
-#                        raise ValueError(f"survey_ranges can't be found in dictionary.")
-#                    tc.append(dt['survey_ranges'][idx])
-#            if any([c=='survey_width_ratio' for c in self.survey_info_to_include]):
-#                if not 'survey_ranges' in dt:
-#                    raise ValueError(f"survey_ranges can't be found in dictionary.")
-#                survey_width_ratio = np.array([(r[0][1]-r[0][0])/(r[1][1]-r[1][0]) for r in dt['survey_ranges']])
-#                survey_width_ratio = np.expand_dims(survey_width_ratio, axis=1)
-#                tc.append(survey_width_ratio)
-#            if any([l=='noise_scale' for l in self.survey_info_to_include]):
-#                tc.append(noise_scale_sampled)
-#
-#        td = [np.expand_dims(dt[k], axis=1) for k in self.model_parameters_to_include]
-#        return td, tc
-#
-
 
