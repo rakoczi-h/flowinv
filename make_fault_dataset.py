92062ee2be23a774cc7b5e4d1a96e13375af5e7a
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2025-08-11 12:57:08 +0200

    More parameters inferred

--- make_fault_dataset.py
+++ make_fault_dataset.py
@@ -13,26 +13,29 @@ start_time = datetime.now()
 n = int(sys.argv[1])
 
 # Specifying directories
-save = '/scratch/balta0/2263373r/fault_python/synthetic_inversion_v3/'
+save = './inversion_dataset_10_dim/'
 if not os.path.exists(save):
     os.mkdir(save)
 
 # Priors
 distributions = {'cx': ['Uniform', -1.0, 1.0],
                  'cy': ['Uniform', -1.0, 1.0],
-                 'l': ['Uniform', 1.0, 2.0],
-                 'cz': ['Uniform', 0.1, 0.2],
-                 'alpha': ['Uniform', 0.0, 2*np.pi]
-                 #'density': ['Uniform', 500.0, 1000.0],
-                 #'DL_ratio': ['Uniform', 0.01, 0.05]
+                 'l': ['Uniform', 1.0, 4.0],
+                 'cz': ['Uniform', 0.01, 0.2],
+                 'alpha': ['Uniform', 0.0, 2*np.pi],
+                 'density': ['Uniform', 500.0, 1000.0],
+                 'DL_ratio': ['Uniform', 0.01, 0.05],
+                 'dip': ['Uniform', 30*np.pi/180, 80*np.pi/180],
+                 'Blend_order': ['Uniform', 0.3, 1.8],
+                  'Displacement_order': ['Uniform', 1.2, 2.4]
                  }
 priors = Prior(distributions=distributions)
 
 # Defining the source model framework
 model_framework = {
     "type": 'parameterised',
-    'default_parameters': {'dip': 70*np.pi/180, 'density': 800.0, 'DL_ratio': 0.02},
-    'varied_parameters': ['cx', 'cy', 'cz', 'l', 'alpha']
+    'default_parameters': None,
+    'varied_parameters': ['cx', 'cy', 'cz', 'l', 'alpha', 'density', 'DL_ratio', 'dip', 'Blend_order', 'Displacement_order']
 }
 
 # Defining the gravimetry survey framework
@@ -46,70 +49,70 @@ survey_framework = {
 }
 
 
-# Training data
-#size = 10000 # Only making a small batch here, in reality we will likely need more data than this.
-#dt_train = FaultDataset(
-#   priors = priors,
-#   size = size,
-#   survey_framework = survey_framework,
-#   model_framework = model_framework
-#)
-#dt_train.make_dataset(augment=False, window=False, pad=False, num_components=50)
-#
-#
+#Training data
+size = 1000 # Only making a small batch here, in reality we will likely need more data than this.
+dt_train = FaultDataset(
+  priors = priors,
+  size = size,
+  survey_framework = survey_framework,
+  model_framework = model_framework
+)
+dt_train.make_dataset(augment=True, augment_dims=['density', 'depth'], augment_num=10, window=True, zero_pad=True, num_components=100)
+
+
 #filename = os.path.join(save, f"trainset_{n}.pkl")
 #with open(filename, 'wb') as file:
-#    pkl.dump(dt_train, file)
-#
-#print(f"Memory use in gb: ", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1e6+resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss/1e6)
-#print(f"Time taken: {datetime.now()-start_time}")
-#
-#
-## Validation data
-#size = 1000 # Only making a small batch here, in reality we will likely need more data than this.
-#dt_train = FaultDataset(
+#   pkl.dump(dt_train, file)
+
+print(f"Memory use in gb: ", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1e6+resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss/1e6)
+print(f"Time taken: {datetime.now()-start_time}")
+
+
+# # Validation data
+# size = 1000 # Only making a small batch here, in reality we will likely need more data than this.
+# dt_train = FaultDataset(
 #   priors = priors,
 #   size = size,
 #   survey_framework = survey_framework,
 #   model_framework = model_framework
-#)
-#dt_train.make_dataset(augment=False, window=False, pad=False, num_components=50)
-#
-#
-#filename = os.path.join(save, f"validationset_{n}.pkl")
-#with open(filename, 'wb') as file:
+# )
+# dt_train.make_dataset(augment=True, augment_dims=['density'], augment_num=10, window=True, zero_pad=True, num_components=100)
+
+
+# filename = os.path.join(save, f"validationset_{n}.pkl")
+# with open(filename, 'wb') as file:
 #    pkl.dump(dt_train, file)
 
-# PP data
-size = 100 # Only making a small batch here, in reality we will likely need more data than this.
-dt_train = FaultDataset(
-   priors = priors,
-   size = size,
-   survey_framework = survey_framework,
-   model_framework = model_framework
-)
-dt_train.make_dataset(augment=False, window=False, pad=False, num_components=50)
+# # PP data
+# size = 100 # Only making a small batch here, in reality we will likely need more data than this.
+# dt_train = FaultDataset(
+#    priors = priors,
+#    size = size,
+#    survey_framework = survey_framework,
+#    model_framework = model_framework
+# )
+# dt_train.make_dataset(augment=False, window=False, pad=False, num_components=50)
 
 
-filename = os.path.join(save, f"ppset_{n}.pkl")
-with open(filename, 'wb') as file:
-    pkl.dump(dt_train, file)
+# filename = os.path.join(save, f"ppset_{n}.pkl")
+# with open(filename, 'wb') as file:
+#     pkl.dump(dt_train, file)
 
 
-# Test data
-size = 10 # Only making a small batch here, in reality we will likely need more data than this.
-dt_train = FaultDataset(
-   priors = priors,
-   size = size,
-   survey_framework = survey_framework,
-   model_framework = model_framework
-)
-dt_train.make_dataset(augment=False, window=False, pad=False, num_components=50)
+# # Test data
+# size = 10 # Only making a small batch here, in reality we will likely need more data than this.
+# dt_train = FaultDataset(
+#    priors = priors,
+#    size = size,
+#    survey_framework = survey_framework,
+#    model_framework = model_framework
+# )
+# dt_train.make_dataset(augment=False, window=False, pad=False, num_components=50)
 
 
-filename = os.path.join(save, f"testset_{n}.pkl")
-with open(filename, 'wb') as file:
-    pkl.dump(dt_train, file)
+# filename = os.path.join(save, f"testset_{n}.pkl")
+# with open(filename, 'wb') as file:
+#     pkl.dump(dt_train, file)
 
 
 #survey_framework = {
