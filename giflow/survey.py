941e1c0f4ccd2574cf9415da3db34578594ba1a1
Henrietta Rakoczi <rakoczi.henrietta@hotmail.com>
2025-08-24 08:19:00 +0100

    Changed survey plotting style

--- giflow/survey.py
+++ giflow/survey.py
@@ -259,22 +259,23 @@ class GravitySurvey():
             plot_data = self.gravity
         gravity_range = self.gravity.max()-self.gravity.min()
         levels = np.linspace(self.gravity.min(), self.gravity.max()+gravity_range/20, 256)
+        levels_2 = np.linspace(self.gravity.min(), self.gravity.max()+gravity_range/20, 4)
         cmap = 'plasma'
         norm = matplotlib.colors.Normalize(vmin=self.gravity.min(), vmax=self.gravity.max()+gravity_range/20)
         fig, ax = plt.subplots()
         ax.plot(self.survey_coordinates[:,0], self.survey_coordinates[:,1], 'o', markersize=2, color='black')
         ax.tricontourf(self.survey_coordinates[:,0], self.survey_coordinates[:,1], plot_data, levels=levels, cmap=cmap, norm=norm)
-        ax.tricontour(self.survey_coordinates[:,0], self.survey_coordinates[:,1], plot_data, levels=4, colors='k', linewidths=0.2)
+        ax.tricontour(self.survey_coordinates[:,0], self.survey_coordinates[:,1], plot_data, levels=levels_2, colors='k', linewidths=0.2)
         if axis_limits is not None:
             ax.set_xlim(left=axis_limits[0], right=axis_limits[1])
             ax.set_ylim(bottom=axis_limits[2], top=axis_limits[3])
         else:
             ax.set(xlim=(np.min(self.survey_coordinates[:,0]), np.max(self.survey_coordinates[:,0])), ylim=(np.min(self.survey_coordinates[:,1]), np.max(self.survey_coordinates[:,1])))
-        ax.set_ylabel('y', fontsize=14)
-        ax.set_xlabel('x', fontsize=14)
+        ax.set_ylabel('y [m]', fontsize=16)
+        ax.set_xlabel('x [m]', fontsize=16)
         ax.set_aspect(aspect='equal')
         cbar = plt.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
-        cbar.set_label(label=r'$\Delta$g [$\mu$Gal]', fontsize=14)
+        cbar.set_label(label=r'$\Delta$g [$\mu$Gal]', fontsize=16)
         plt.savefig(filename)
         plt.close()
 
